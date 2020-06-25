fbfb15c59661a8ec236ea5b8992d88aeab673402
shah-deep <56083928+shah-deep@users.noreply.github.com>
2020-06-26 00:16:49 +0530

    small change

--- DBMS/Agent/ManagePolicy/DeletePolicy.php
+++ DBMS/Agent/ManagePolicy/DeletePolicy.php
@@ -5,7 +5,7 @@ require_once '../header.php';
   <h1>Delete Policy from Record</h1>
   <form class="" action="../includes/DeletePolicy-inc.php" method="post">
     <input type="number" name="Policy_no" placeholder="Policy Number" min="100000000" max="999999999" required>
-    <input type="submit" name="submit" onclick="func()">
+    <input type="submit" name="submit"">
   </form>
   <br><br>
   <p> <b>Warning:</b> This will also Delete All Payment_records associated with this Policy_no.</p>
